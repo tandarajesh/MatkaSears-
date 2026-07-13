2dcf0b48d1a252a5ed70dcf0af7bbad27305fc63
Rajesh Tanda <mr.rajeshtanda@gmail.com>
2026-07-13 17:59:09 +0530

    Added JWT authentication and login API

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
+from app.core.auth import create_access_token
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -12,13 +13,44 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = User(
-    username=user.username,
-    email=user.email,
-    password=hash_password(user.password)
-)
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
 
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
 
     return db_user
+
+
+@router.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
